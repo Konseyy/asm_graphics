39d3702953938961ef84b6aeafe929e203c53863
Valdis Guntis Bukalders <valdis.bukalders@giraffe360.com>
2023-12-14 20:35:40 +0200

    update

--- agra_main.c
+++ agra_main.c
@@ -1,10 +1,24 @@
 #include "agra.h"
 
-
-int main() {
+void pixel(int x, int y, pixcolor_t *color)
+{
   pixcolor_t *frameBuffer = FrameBufferGetAddress();
   int frameWidth = FrameBufferGetWidth();
   int frameHeight = FrameBufferGetHeight();
+  if (x >= 0 && x < frameWidth && y >= 0 && y < frameHeight)
+  {
+    frameBuffer[y * frameWidth + x] = *color;
+  }
+}
+
+int main()
+{
+  pixcolor_t *frameBuffer = FrameBufferGetAddress();
+  int frameWidth = FrameBufferGetWidth();
+  int frameHeight = FrameBufferGetHeight();
+
+  pixel(0, 0, &(pixcolor_t){1000, 0, 1000, 0});
+  pixel(27, 4, &(pixcolor_t){0, 550, 1000, 0});
 
   FrameShow();
   return 0;

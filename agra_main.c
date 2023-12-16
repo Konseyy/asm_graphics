7fec42f9aa7a51c7750250d57719c266cfc3862a
Valdis Guntis Bukalders <valdis.bukalders@giraffe360.com>
2023-12-16 19:33:15 +0200

    asd

--- agra_main.c
+++ agra_main.c
@@ -1,5 +1,4 @@
 #include "agra.h"
-#include <stdio.h>
 
 int main()
 {
@@ -10,17 +9,16 @@ int main()
   // pixel(0, 0, &(pixcolor_t){1000, 0, 1000, 0});
   // pixel(27, 4, &(pixcolor_t){0, 550, 1000, 0});
 
-
-  for(int i=0; i< frameWidth; i++)
+  for (int i = 0; i < frameWidth; i++)
   {
     for (int j = 0; j < frameHeight; j++)
     {
-      printf("%d %d\n", i, j);
       pixel(j, i, &(pixcolor_t){1, 1, 1, 0});
+      FrameShow();
     }
   }
 
-    pixel(0, 0, &(pixcolor_t){1, 0, 0, 0});
+  pixel(0, 0, &(pixcolor_t){1, 0, 0, 0});
   pixel(1, 0, &(pixcolor_t){0, 1, 0, 0});
   pixel(0, 1, &(pixcolor_t){0, 0, 1, 0});
   pixel(0, 2, &(pixcolor_t){0, 0, 1, 0});

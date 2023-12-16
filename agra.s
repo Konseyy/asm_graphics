a5c30ffa47b41d5ea5b40f00006d71ec1a8d80fc
Valdis Guntis Bukalders <valdis.bukalders@giraffe360.com>
2023-12-16 20:28:47 +0200

    asd

--- agra.s
+++ agra.s
@@ -66,17 +66,40 @@ line:
   mov r10, r0 // r10 = height
   bl FrameBufferGetAddress // r0 = framebuffer base address
   mov r11, r0 // r11 = framebuffer base address
-  ldr r12, [r11] // r12 = current color
+  @ mov r0, r5
+  @ mov r1, r6
+  @ mov r2, r11
+  @ bl pixel // draw first pixel
+  @ mov r0, r7
+  @ mov r1, r8
+  @ mov r2, r11
+  @ bl pixel // draw second pixel
+  @ b end
+  sub r0, r7, r5 // r0 = delta x
+  sub r1, r8, r6 // r1 = delta y
+  // y1 coordinate no longer needed as we have the slope
+  mov r8, r0 // r7 = delta x
+  mov r12, r1 // r12 = delta y
+  // slope = r8 / r12
+
+line_loop:
+  cmp r5, r6 // if x0 >= x1
+  bgt end
   mov r0, r5
   mov r1, r6
   mov r2, r11
-  bl pixel // draw first pixel
-  mov r0, r7
+  bl pixel // draw pixel
+  add r5, r5, #1 // x0++
+  mul r0, r6, r8 // y0 * delta x
+  add r0, r0, r12
   mov r1, r8
-  mov r2, r11
-  bl pixel // draw second pixel
-  b end
+  bl divide // (y0 * d_x + d_y) / delta x
+  mov r6, r0 // y0 = (y0 * d_x + d_y) / delta x
+  b line_loop
 
+end:
+  ldmfd sp!, {r5-r12, lr}
+  bx lr // return
 
 @ (x, y) returns x/y
 divide:

95d58601c00c84127f1a2733e7a977b4a67a634f
Valdis Guntis Bukalders <valdis.bukalders@giraffe360.com>
2023-12-16 23:54:53 +0200

    dddaaa

--- agra.s
+++ agra.s
@@ -104,20 +104,20 @@ line_loop:
 @ (x, y) returns x/y
 divide:
   stmfd sp!, {r5-r12, lr}
-  @ r0 = dividend, r1 = divisor
-  @ Result will be placed in r0
-  stmfd sp!, {r5-r12, lr}
-  mov r1, r0
-  ldr r0, f__i
-  bl printf
-  ldmfd sp!, {r5-r12, lr}
-  stmfd sp!, {r5-r12, lr}
-  ldr r0, f__i
-  bl printf
-  ldr r0, f__i
-  mov r1, #-111
-  bl printf
-  ldmfd sp!, {r5-r12, lr}
+  @ @ r0 = dividend, r1 = divisor
+  @ @ Result will be placed in r0
+  @ stmfd sp!, {r5-r12, lr}
+  @ mov r1, r0
+  @ ldr r0, f__i
+  @ bl printf
+  @ ldmfd sp!, {r5-r12, lr}
+  @ stmfd sp!, {r5-r12, lr}
+  @ ldr r0, f__i
+  @ bl printf
+  @ ldr r0, f__i
+  @ mov r1, #-111
+  @ bl printf
+  @ ldmfd sp!, {r5-r12, lr}
   @ Check for divisor = 0 to avoid division by zero
   cmp r1, #0
   beq end

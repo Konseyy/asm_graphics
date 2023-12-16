bffea710e38b6992b61dd2cf1296013c4b3e620a
Valdis Guntis Bukalders <valdis.bukalders@giraffe360.com>
2023-12-16 18:22:36 +0200

    s

--- agra.s
+++ agra.s
@@ -7,9 +7,9 @@ pixel:
   mov r5, r0 // r5 = x
   mov r6, r1 // r6 = y
   mov r7, r2 // r7 = color pointer
-  bx FrameBufferGetWidth
+  bl FrameBufferGetWidth
   mov r8, r0 // r8 = width
-  bx FrameBufferGetHeight
+  bl FrameBufferGetHeight
   mov r9, r0 // r9 = height
   cmp r5, r8 // if x >= width
   bge end
@@ -19,11 +19,12 @@ pixel:
   blt end
   cmp r6, #0 // if y < 0
   blt end
-  mul r5, r5, #4 // x *= 4
+  mov r0, #4
+  mul r5, r5, r0 // x *= 4
   mul r6, r6, r8 // y *= width
   add r5, r5, r6 // x += y
 
-  bx FrameBufferGetAddress // r0 = framebuffer base address
+  bl FrameBufferGetAddress // r0 = framebuffer base address
   // Calculate the pixel address in r5
   ADD r5, r0, r5           // Add the offset to the base address to get the pixel address
 

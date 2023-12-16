5c7290621ccb19463aa9ad2cecd09d3dde3ca281
Valdis Guntis Bukalders <valdis.bukalders@giraffe360.com>
2023-12-16 19:21:31 +0200

    asd

--- agra_main.c
+++ agra_main.c
@@ -8,9 +8,9 @@ int main()
 
   pixel(0, 0, &(pixcolor_t){1000, 0, 1000, 0});
   pixel(27, 4, &(pixcolor_t){0, 550, 1000, 0});
-  pixel(0, 0, &(pixcolor_t){0, 550, 0, 0});
-  pixel(1, 0, &(pixcolor_t){1000, 550, 1000, 0});
-  pixel(0, 1, &(pixcolor_t){1000, 550, 0, 0});
+  pixel(0, 0, &(pixcolor_t){1, 0, 0, 0});
+  pixel(1, 0, &(pixcolor_t){0, 1, 0, 0});
+  pixel(0, 1, &(pixcolor_t){0, 0, 1, 0});
 
   FrameShow();
   return 0;

5722bcf6985fbb1afc6ff4c322faa824aa851e10
Valdis Guntis Bukalders <valdis.bukalders@giraffe360.com>
2023-12-17 18:50:08 +0200

    test

--- agra.s
+++ agra.s
@@ -143,6 +143,15 @@ triangleFill:
 
   mov r11, r1 // r11 = y_min
 
+  stmfd sp!, {r0-r3} // save x_min, y_min, x_max, y_max
+  mov r1, r0
+  ldr r0, f__x
+  bl printf
+  mov r1, r11
+  ldr r0, f__y
+  bl printf
+  ldmfd sp!, {r0-r3} // restore x_min, y_min, x_max, y_max
+
 for_x:
   cmp r0, r2 // if x_min > x_max
   bgt after_loop // finish loop
